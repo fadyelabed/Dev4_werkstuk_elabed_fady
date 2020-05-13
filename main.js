f29d350a966880c47965b974ef589ac49119b4f8
Fady Elabed <fady@MacBook-Pro-van-Fady-2.local>
2020-05-13 16:53:46 +0200

    id's meegegeven aan filters

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 "use strict";
-$( document ).ready(function() {
-        console.log( "ready!" );
-            fetch("data.json").then(function(resp) {
-                return resp.json();
-            })
-            .then(function(data) {
-            
-                getData(data.items);
-            });
-            let name = data.items.name;
-            console.log(name);
+$(document).ready(function() {
+    console.log("ready!");
+    fetch("data.json").then(function(resp) {
+            return resp.json();
+        })
+        .then(function(data) {
 
-            function getData(items){
-            
-                for(let i of items){
-                    let category = i.category;
-                    let thumbnail = i.thumbnail.url;
-                    let name = i.name;
-                    // let recorded = i.recorded-at;
-                    let videolength = i.video-length;
+            getData(data.items);
+        });
+    // let name = data.items.name;
+    // console.log(name);
 
-                    console.log(i);
-                    $(".events").append(`<section style="background-image: url('${thumbnail}');"><h2>${category}</h2><article><h1>${name}</h1></article></section>`)
-                }
-            }
+    function getData(items) {
+
+        for (let i of items) {
+            let category = i.category;
+            let genre = i["genre-v2"];
+            let thumbnail = i.thumbnail.url;
+            let name = i.name;
+            // let recorded = i.recorded-at;
+            let videolength = i.video - length;
+
+            $(".events").append(`<section class="my_cards ${category} ${genre}" style="background-image: url('${thumbnail}');">
+            <h2>${category}</h2>
+            <article><h1>${name}</h1>
+            <h3>${genre}</h3>
+            </article></section>`)
+        }
+    }
+
+
 });
